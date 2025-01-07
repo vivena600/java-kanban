ce15721ca0c1829b05972e75a0fe0dd8a1b31f2f
Даша <golovinad84@gmail.com>
2025-01-07 19:28:53 +0500

    Исправила ошибки связанные с CodeStyle

--- src/controlles/Node.java
+++ src/controlles/Node.java
@@ -1,11 +1,11 @@
 package controlles;
 
-public class Node<Task> {
-    Node<Task> next;
-    Node<Task> last;
-    Task task;
+public class Node<T> {
+    Node<T> next;
+    Node<T> last;
+    T task;
 
-    public Node(Node<Task> last, Task task,Node<Task> next) {
+    public Node(Node<T> last, T task, Node<T> next) {
         this.next = next;
         this.task = task;
         this.last = last;
