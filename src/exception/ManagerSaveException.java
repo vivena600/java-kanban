16f71911c26acf1759ed6ec6bdaf947d8b49bedb
Даша <golovinad84@gmail.com>
2025-01-21 14:04:45 +0500

    Исправлены ошибки CodeStyle

--- src/exception/ManagerSaveException.java
+++ src/exception/ManagerSaveException.java
@@ -5,6 +5,7 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException() {
 
     }
+
     public ManagerSaveException(final String message) {
         super(message);
     }
