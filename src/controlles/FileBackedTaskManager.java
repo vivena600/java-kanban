de6d3bd3d692bdd93e31c2cdecb255e6e87527f3
Даша <golovinad84@gmail.com>
2025-01-21 13:55:23 +0500

    Исправлены ошибки в FileBackedTaskManager и добавлены тесты

--- src/controlles/FileBackedTaskManager.java
+++ src/controlles/FileBackedTaskManager.java
@@ -41,15 +41,15 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     public static Task fromString(String line) {
         String[] params = line.split(",");
         int id = Integer.parseInt(params[0]);
-        String type = params[1];
+        TypeTask type = TypeTask.valueOf(params[1]);
         String title = params[2];
         TaskStatus status = TaskStatus.valueOf(params[3]);
         String description = params[4];
 
-        if (type.equals(TypeTask.TASK)) {
+        if (type == TypeTask.TASK) {
             Task task = new Task(title, description, status, id);
             return task;
-        } else if (type.equals(TypeTask.EPIC)) {
+        } else if (type == TypeTask.EPIC) {
             Epic epic = new Epic(title, description, status, id);
             return epic;
         } else {
@@ -98,35 +98,30 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     }
 
     public static void main(String[] args) {
-        try {
-            File file = File.createTempFile("mainBackedTaskManager", ".csv");
-            FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file.getName());
-            Task task1 = new Task("Задача 1", "Описание 1"); //id = 1
-            Task task2 = new Task("Задача 2", "Описание 2"); //id = 2
-            fileBackedTaskManager.add(task1);
-            fileBackedTaskManager.add(task2);
-            Epic epic1 = new Epic("Эпик 1", "эпик с 3 подзадачами"); //id = 3
-            Epic epic2 = new Epic("Эпик 2", "эпик без задач"); //id = 4
-            fileBackedTaskManager.add(epic1);
-            fileBackedTaskManager.add(epic2);
-            SubTask subTask1 = new SubTask("подзадача 1", "описание 1", epic1.getId()); //id = 5
-            SubTask subTask2 = new SubTask("подзадача 2", "описание 2", epic1.getId()); //id = 6
-            SubTask subTask3 = new SubTask("подзадача 3", "описание 3", epic1.getId()); //id = 7
-            fileBackedTaskManager.add(subTask1);
-            fileBackedTaskManager.add(subTask2);
-            fileBackedTaskManager.add(subTask3);
-            System.out.println(file.getName());
+        File file = new File("test.csv");
+        FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file.getName());
+        Task task1 = new Task("Задача 1", "Описание 1"); //id = 1
+        Task task2 = new Task("Задача 2", "Описание 2"); //id = 2
+        fileBackedTaskManager.add(task1);
+        fileBackedTaskManager.add(task2);
+        Epic epic1 = new Epic("Эпик 1", "эпик с 3 подзадачами"); //id = 3
+        Epic epic2 = new Epic("Эпик 2", "эпик без задач"); //id = 4
+        fileBackedTaskManager.add(epic1);
+        fileBackedTaskManager.add(epic2);
+        SubTask subTask1 = new SubTask("подзадача 1", "описание 1", epic1.getId()); //id = 5
+        SubTask subTask2 = new SubTask("подзадача 2", "описание 2", epic1.getId()); //id = 6
+        SubTask subTask3 = new SubTask("подзадача 3", "описание 3", epic1.getId()); //id = 7
+        fileBackedTaskManager.add(subTask1);
+        fileBackedTaskManager.add(subTask2);
+        fileBackedTaskManager.add(subTask3);
+        Task task2Update = new Task("Задача 2", "Описание 2", TaskStatus.IN_PROGRESS, 2);
+        fileBackedTaskManager.update(task2Update);
 
-            FileBackedTaskManager newFileBacked = loadFromFile(file);
-            Task task3 = new Task("Задача 3", "Описание 3"); //id = 8
-            Task task1Update = new Task("Задача 1", "Описание 1", TaskStatus.DONE, 1);
-            newFileBacked.update(task1Update);
-            newFileBacked.add(task3);
-            newFileBacked.save();
-            System.out.println(file.getName());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        FileBackedTaskManager newFileBacked = loadFromFile(file);
+        Task task3 = new Task("Задача 3", "Описание 3"); //id = 8
+        Task task1Update = new Task("Задача 1", "Описание 1", TaskStatus.DONE, 1);
+        newFileBacked.update(task1Update);
+        newFileBacked.add(task3);
     }
 
     @Override
@@ -150,6 +145,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     @Override
     public void update(SubTask subTask) {
         super.update(subTask);
+        save();
     }
 
     @Override
